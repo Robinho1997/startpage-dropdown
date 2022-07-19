037cf9a41353ce31f26909ed5367db054b8f171a
Robinho1997 <robin.kempen@gmx.de>
2022-07-19 18:46:24 +0200

    startpage-dropdown

---
+++ script.js
@@ -0,0 +1,7 @@
+function showFeatures() {
+   document.getElementById("dropdown-features").classList.toggle("show");
+}
+
+function showCompany() {
+    document.getElementById("dropdown-company").classList.toggle("show");
+}
